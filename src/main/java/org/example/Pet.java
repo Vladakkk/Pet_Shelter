53b43a952de4e9f7bf169b0e68300f2579a1e6b5
Vladakkk <vladakasperskaa@gmail.com>
2024-05-21 21:39:19 +0300

    pet_shelter

---
+++ src/main/java/org/example/Pet.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Pet {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Pet(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
